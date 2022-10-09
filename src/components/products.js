c470676e0c73193182be112cbd5dd42dcf5b79d6
Ryuu <85mvrtin@gmail.com>
2022-10-09 20:32:55 -0300

    Fix errors

--- src/components/products.js
+++ src/components/products.js
@@ -2,52 +2,73 @@ const { products } = require('../../data/products.json')
 
 module.exports = {
   getProducts: (req, res) => {
-    res.json({ products })
+    if (products !== '{}') {
+      res.json({ products })
+    } else return res.send('No products')
   },
 
   getProduct: (req, res) => {
     const { id } = req.params
-
-    res.json(products[id])
+    try {
+      res.json(products[id])
+    } catch (err) {
+      res.send({ products: null })
+    }
   },
 
   addProduct: (req, res) => {
     const { name, price, stock } = req.body
-    products.push({
-      id: products.length,
-      name,
-      price,
-      stock
-    })
 
-    res.json({ success: true })
+    if (!name || price || stock) return res.send({ error: 'Parameters' })
+    try {
+      products.push({
+        id: products.length,
+        name,
+        price,
+        stock
+      })
+
+      res.json({ success: true })
+    } catch (err) {
+      res.json({ error: 'Misterio' })
+    }
   },
 
   updateProduct: (req, res) => {
     const { id } = req.params
     const { name, price, stock } = req.body
-    products.forEach((product, i) => {
-      if (product.id === Number(id)) {
-        product.name = name
-        product.price = price
-        product.stock = stock
-      }
-    })
 
-    res.json({ success: true })
+    if (!name || !price || !stock || !id) return res.send({ error: 'Parameters' })
+    try {
+      products.forEach((product, i) => {
+        if (product.id === Number(id)) {
+          product.name = name
+          product.price = price
+          product.stock = stock
+        }
+      })
+
+      res.json({ success: true })
+    } catch (err) {
+      res.json({ products: null })
+    }
   },
 
   deleteProduct: (req, res) => {
     const id = req.params.id
 
-    const found = false
+    if (!id) return res.json({ products: null })
 
-    products.forEach(function (product, index) {
-      if (!found && product.id === Number(id)) {
-        products.splice(index, 1)
-      }
-    })
-    res.json({ success: true })
+    try {
+      products.forEach(function (product, index) {
+        if (product.id === Number(id)) {
+          products.splice(index, 1)
+        }
+      })
+      res.json({ success: true })
+    } catch (err) {
+      res.json({ products: null })
+    }
   }
 
 }
